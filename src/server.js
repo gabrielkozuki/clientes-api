8f3e39118aa09c823259d0fa20d7eb1eca75196c
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2026-02-28 12:30:59 -0300

    feat: adiciona documentação Swagger na rota /docs com dependências swagger-ui-express e js-yaml

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
 
 import { connectToDatabase } from './db.js';
 import clientRoutes from './routes/clientRoutes.js';
@@ -8,9 +11,12 @@ import { errorHandler } from './middlewares/errorHandler.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const swaggerDocument = load(readFileSync('./src/swagger.yml', 'utf8'));
+
 app.use(express.json());
 
 app.use(clientRoutes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandler);
 
 try {
